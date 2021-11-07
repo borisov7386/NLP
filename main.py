b965b16ce153c437a51fefbdb8be77ab63b2a024
zuxel <a.borisov7386@gmail.com>
2021-11-07 12:50:10 +0700

    111

--- main.py
+++ main.py
@@ -10,24 +10,8 @@ import nltk
 
 texts = ["нихренашеньки ваши роутеры не работает у него", "нихренашеньки ваши роутеры не работает у нас",
          "нихренашеньки ваши роутеры не работает у вас"]
-ww= ""
 
-for text in texts:
-    p = nltk.word_tokenize(text)
-    morph = pymorphy2.MorphAnalyzer()
-    for word in p:
-        d = morph.parse(word)[0]
-        ww = ww + "\n"+ d.normal_form
-
-print(ww)
-
-# text = "нихренашеньки ваши роутеры не работает у него"
-#
-# p = nltk.word_tokenize(text)
-# print(p)
-#
-# morph=pymorphy2.MorphAnalyzer()
-#
-# for word in p:
-#      d = morph.parse(word)[0]
-#      print(d.normal_form)
+m = pymorphy2.MorphAnalyzer()
+lem_phrasez = [" ".join([pymorphy2.MorphAnalyzer().parse(word)[0].normal_form for word in text.split()]) for text in texts]
+# print(lem_phrasez)
+print('\n'.join(lem_phrasez))
